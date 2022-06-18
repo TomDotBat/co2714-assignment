3cf7916297b98f815981f2931c906eb4f04ac15b
Tom O'Sullivan <tom@tomdotbat.dev>
2022-06-18 17:07:57 +0100

    refactor: change the admin dashboard home message

--- resources/js/Pages/Admin/index.jsx
+++ resources/js/Pages/Admin/index.jsx
@@ -2,15 +2,21 @@ import Dashboard from "./Dashboard";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import {usePage} from "@inertiajs/inertia-react";
 
 export default function Admin() {
+    const user = usePage().props.auth.user;
+
     return (
         <Dashboard>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
                     <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                        <Typography>
-                            hello
+                        <Typography variant="h5">
+                            Welcome to the admin dashboard, {user.name}!
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            Use the navigation drawer on the left to start managing your business.
                         </Typography>
                     </Paper>
                 </Grid>
